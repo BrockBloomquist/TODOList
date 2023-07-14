1308d554b71989f386fdfef5102370e408a1a8e2
Brock Bloomquist <brockbloomquist1@gmail.com>
2023-07-14 12:19:22 -0500

    Added the handleSubmit function and created a constant to assign the newItem value to within the form input. Need to add the handling of information

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,34 @@
 import "./styles.css";
+import { useState } from "react";
 
 export default function App() {
+  const [newItem, setNewItem] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+  }
   return (
-    <form className="new-item-form">
-      <div className="form-row">
-        <label htmlFor="item">New Item</label>
-        <input type="text" id="item" />
-        <button className="btn">Add</button>
-      </div>
-    </form>
+    <>
+      <form className="new-item-form" onSubmit={handleSubmit}>
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+            id="item"
+          />
+          <button className="btn">Add</button>
+        </div>
+      </form>
+      <h1 className="header">TODO List</h1>
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
+            Item 1<button className="btn btn-danger">Delete</button>
+          </label>
+        </li>
+      </ul>
+    </>
   );
 }
