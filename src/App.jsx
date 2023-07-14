f226c859f231f80a0025b2173ddde3a12ca9ebf4
Brock Bloomquist <brockbloomquist1@gmail.com>
2023-07-14 13:16:55 -0500

    Todo list completed for the most part. Now time to style and make it look better. Able to check the todos on the list and delete them

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,22 @@ export default function App() {
       ];
     });
   }
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
+    });
+  }
 
   return (
     <>
@@ -30,13 +46,23 @@ export default function App() {
       </form>
       <h1 className="header">TODO List</h1>
       <ul className="list">
+        {todos.length === 0 && "No Todos"}
         {todos.map((todo) => {
           return (
-            <li>
+            <li key={todo.id}>
               <label>
-                <input type="checkbox" checked={todo.completed} />
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
                 {todo.title}
-                <button className="btn btn-danger">Delete</button>
+                <button
+                  onClick={() => deleteTodo(todo.id)}
+                  className="btn btn-danger"
+                >
+                  Delete
+                </button>
               </label>
             </li>
           );
